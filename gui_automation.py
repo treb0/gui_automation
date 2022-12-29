24f5e784e71559b0d460cb3b9914c9ec10b69bca
treb0 <mtrebinonixon@gmail.com>
2022-12-29 14:33:36 -0300

    first working version

--- gui_automation.py
+++ gui_automation.py
@@ -1,13 +1,13 @@
 import pyautogui
 import pandas as pd
+import numpy as np
 import sys
+import time
+from datetime import datetime
+import os
 
 # global variables
 programme_exit = False
-
-# import csv
-flows_df = pd.read_csv('flows.csv')
-actions_df = pd.read_csv('flows.csv')
 
 def programme_exit_10_s():
     print('')
@@ -15,28 +15,68 @@ def programme_exit_10_s():
     time.sleep(10)
     sys.exit('')
 
+def any_key_to_exit():
+    print('')
+    any_key = input('Press enter to exit programme')
+    sys.exit('')
+
 def clean_string(string):
 
-    return string.lower().lstrip(' ','').rstrip(' ','')
+    if type(string) == str:
+
+        return string.lower().lstrip(' ').rstrip(' ')
+
+    else:
+        
+        if np.isnan(string):
+
+            return ''
+
+        else:
+
+            return float(string)
+        
 
 
-def execute_action(action_name):
+def execute_action(data_i
+                  ,flow_i
+                  ,flow_row
+                  ,data_row):
 
-    # find action in df
+    print('')
 
-    try:
-        i = actions_df.loc[actions_df['name']==action_name].index.tolist()[0]
-    except:
-        print(f'ERROR: Cannot find action "{action_name}" in actions.csv')
+    action = clean_string(flow_row['action'])
+    location_method = clean_string(flow_row['location_method'])
+    before_sleep_s = clean_string(flow_row['before_sleep_s'])
+    after_sleep_s = clean_string(flow_row['after_sleep_s'])
+    x = clean_string(flow_row['x'])
+    y = clean_string(flow_row['y'])
+    image_name = clean_string(flow_row['image_name'])
+    text = clean_string(flow_row['text'])
+    interval_s = clean_string(flow_row['interval_s'])
+    if interval_s == '': interval_s = 0.25
+    
+    # detect if text is data.csv data
+    if '{' in text:
+        data_column = text.replace('{','').replace('}','')
+        text = str(data_row[data_column])
 
-        programme_exit_10_s()
 
-    action = clean_string(actions_df['action'][i])
-    location_method = clean_string(actions_df['location_method'][i])
-    x = int(clean_string(actions_df['x'][i]))
-    y = int(clean_string(actions_df['y'][i]))
-    image_name = clean_string(actions_df['image_name'][i])
-    write_text = clean_string(actions_df['write_text'][i])
+    # before sleep
+    if before_sleep_s != '':
+
+        print(f'''data_row: {data_i+1}, flow_row:{flow_i+1} - action: {action} - executing before sleep of {before_sleep_s}''')
+        time.sleep(int(before_sleep_s))
+
+
+    # print executing action
+
+    if action not in ('','sleep'):
+
+        print(f'''data_row: {data_i+1}, flow_row:{flow_i+1} - action: {action} - executing action''')
+
+
+    # execute action logic
 
     if action == 'move':
 
@@ -46,52 +86,90 @@ def execute_action(action_name):
 
         elif location_method == 'relative coordinates':
 
+            pyautogui.move(x,y, duration = 1)
 
+        elif location_method == 'image':
+
+            # CODE ASDF
+
+            print('CODE Not built yet')
+            any_key_to_exit()
+
+        else:
+            if location_method == '': error_msg = 'empty'
+            else: error_msg = 'unsopported'
+            print(f'''ERROR: {error_msg} location_method: {location_method},, for row: {i} of flow: {flow_name}.csv''')
+            any_key_to_exit()
+
+            
 
 
 
     elif action == 'click':
 
-        pyautogui.moveTo(x,y, duration = 1)
+        pyautogui.click(x
+                       ,y
+                       ,button='left'
+                       ,clicks = 1
+                       ,interval=interval_s
+                       )
 
 
     elif action == 'right click':
 
-        print('EXIT: rigth click action not supported yet. contact developer')
-        programme_exit_10_s()
+        pyautogui.click(x
+                       ,y
+                       ,button='right'
+                       ,clicks = 1
+                       ,interval=interval_s
+                       )
 
 
     elif action == 'write':
 
+        pyautogui.write(text
+                       ,interval=interval_s
+                       )
+
+    elif action == 'hotkey':
+
+        for t in text.split(','):
+
+            pyautogui.hotkey(t)
 
 
-
-
-
-    elif action == 'special keys':
-
-
-
-    elif action == 'pause':
-
-
-
+    elif action == 'sleep': 
+        # ya se corrió en before_sleep_s
+        pass 
 
     elif action == 'screenshot':
 
+        today_str = datetime.now().strftime("%Y_%m_%d")
 
+        now_str = datetime.now().strftime("%Y_%m_%d__%H_%M")
 
+        floder_path = f'screenshots/{today_str}'
 
+        # create today's folder if it does not exist
+        if not os.path.exists(floder_path):
+            os.makedirs(floder_path)
 
+        # capture and save screenshot
 
-
+        pyautogui.screenshot(f'{floder_path}/{now_str}__{data_i}.png')
 
 
     else:
-        print(f'ERROR: Unsoported action: "{action}" for action named: {action_name}')
-        programme_exit_10_s()
+        print(f'ERROR: Unsoported action: "{action}"')
+        any_key_to_exit()
 
 
+    # after sleep
+
+    if after_sleep_s != '':
+
+        print(f'''data_row: {data_i+1}, flow_row:{flow_i+1} - action: {action} - executing after sleep of {after_sleep_s}''')
+        time.sleep(int(after_sleep_s))
 
 
 
@@ -99,28 +177,58 @@ def execute_action(action_name):
 
 # Run main loop
 
+flow_df = []
+data_df = []
+
 while True:
 
-    mode_raw = input('\nChoose programme mode: flow/action/coordinates/exit \n\nflow: run a flow that you have previously entered in flows.csv \naction: run a specific action you have previously entered in actions.csv \ncoordinates: enter the coordinates and the programme will move your mouse to that point\nexit: exit the programme\n\n>>')
+    mode_raw = input('\nChoose programme mode: flow/coordinates/exit \n\nflow: run a flow that you have previously entered in flows.csv \ncoordinates: enter the coordinates and the programme will move your mouse to that point\nexit: exit the programme\n\n>>')
 
     print('')
 
-    mode = mode_raw.replace(' ').lower()
+    mode = mode_raw.replace(' ','').lower()
 
     if mode == 'flow':
 
         # enter flow name
+        print('')
+        flow_name_raw = input('Enter flow name: ')
 
-        # confirm
+        flow_name = flow_name_raw.lstrip(' ').rstrip(' ').replace('.csv','')
+
+        # Get flow csv
+
+        flow_df = pd.read_csv(f'flows/{flow_name}.csv')
+
+        # Get data csv
+
+        if flow_df['action'][0] == 'data csv':
+
+            data_df = pd.read_csv(f'''data/{flow_df['location_method'][0]}''')
+            has_data_csv = True
+            flow_iterations = len(data_df)
+
+        else: 
+
+            has_data_csv = False
+            flow_iterations = 1
+
 
         # execute actions on loop for every row of associated data table
 
+        for data_i in range(flow_iterations):
 
-    elif mode == 'action':
+            data_row = data_df.loc[data_i]
 
-        # enter action name
+            for flow_i,flow_row in flow_df.iterrows():
 
-        # execute that action
+                if has_data_csv & (flow_i == 0): continue
+                    
+                execute_action(data_i
+                              ,flow_i
+                              ,flow_row
+                              ,data_row)
+
 
 
     elif mode == 'coordinates':
@@ -129,7 +237,7 @@ while True:
         
             coordinates_raw = input('Enter "get" to get the coordinates of the mouse\nEnter coordinates in the format of: "x,y"\nEnter "back" to go back to main menu, or "exit" to end the programme\n>>')
 
-            coordinates = coordinates_raw.replace(' '.'').lower()
+            coordinates = coordinates_raw.replace(' ','').lower()
         
             if coordinates == 'exit':
                 
